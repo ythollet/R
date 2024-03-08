7b798e021e8dfd5b9e05fd5ffc68158e5789fe72
ythollet <159007949+ythollet@users.noreply.github.com>
2024-03-08 17:07:45 +0100

    Add files via upload

---
+++ TD 3.R
@@ -0,0 +1,60 @@
+# EXERCICE 1
+# 1
+install.packages("readxl")
+library(readxl)
+df=read_excel(path="pokemon.xlsx",sheet = "pokemon")
+
+#2
+dim(df)
+
+#3
+summary(df)
+
+#4
+df$is_legendary <-as.factor(df$is_legendary)
+df$generation <-as.factor(df$generation)
+df$type <-as.factor(df$type)
+
+#5
+summary(df)
+
+
+# EXERCICE 2
+#1
+df$attack_group=ifelse(df$attack>mean(df$attack),yes="attack+","attack-")
+df$attack_group=as.factor(df$attack_group)
+View(df)
+
+#2
+df$water_fire=ifelse(df$type %in%c("water","fire"),yes="yes",no="no")
+View(df)
+
+#3
+df$best=ifelse(df$attack > c(quantile(df$attack,probs=0.75)) & df$defense > c(quantile(df$defense,probs=0.75)) & df$speed > c(quantile(df$speed,probs=0.75)) ,yes="yes",no="no")
+View(df)
+
+
+#La fonction is.na().
+#1
+requete=subset(df,is.na(weight_kg))
+View(requete)
+
+#1
+requete=subset(df,!is.na(weight_kg))
+View(requete)
+
+#2
+med_weight_kg= median(df$weight_kg,na.rm = TRUE)
+df$weight_kgNa=ifelse(is.na(df$weight_kg),yes=med_weight_kg,no=df$weight_kg)
+                   
+med_height_m= median(df$height_m,na.rm = TRUE)
+df$height_mNA=ifelse(is.na(df$height_m),yes=med_height_m,no=df$height_m)  
+
+View(df)
+
+
+
+#La fonctions cut()
+#1
+df$weight_group=cut(df$weight_kg,breaks=3,labels=c("léger","moyen","lourd"))
+View(df)
